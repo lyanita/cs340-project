c72877314602d1b2ecb49daaef818e8d493a4031
Anita <anitaly.ubc@gmail.com>
2021-07-07 21:41:06 -0700

    Added courses route

--- app.py
+++ app.py
@@ -1,42 +1,23 @@
 # Dependencies
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
+import database.db_connector as db
 
 # Configuration
 app = Flask(__name__)
-
-# Data
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
+db_connection = db.connect_to_database()
 
 # Routes
 @app.route("/")
 def root():
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("main.j2")
+
+@app.route("/courses")
+def courses():
+    query = "SELECT * FROM courses;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("courses.j2", courses=results)
 
 # Listener
 if __name__ == "__main__":
