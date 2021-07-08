49e7461075197f3db431eed76219b0da01b85665
Anita <anitaly.ubc@gmail.com>
2021-07-08 16:34:38 -0700

    Updated SQL queries

---
+++ database/campuses.sql
@@ -0,0 +1,10 @@
+CREATE TABLE campuses(
+    campus_id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    campus_name VARCHAR(255) NOT NULL UNIQUE,
+    campus_city TEXT,
+    campus_country TEXT,
+    campus_online BOOLEAN
+);
+
+INSERT INTO campuses(campus_name, campus_city, campus_country, campus_online)
+VALUES ("Corvallis", "Corvallis", "United States of America", TRUE), ("Davis", "Los Angeles", "United States of America", FALSE), ("Washington", "Seattle", "United States of America", TRUE);
--- database/courses.sql
+++ database/courses.sql
@@ -2,3 +2,6 @@ CREATE TABLE courses(
     course_id INT(10) PRIMARY KEY AUTO_INCREMENT,
     course_name VARCHAR(255) NOT NULL UNIQUE
 );
+
+INSERT INTO courses(course_name)
+VALUES ("Introduction to Databases"), ("Introduction to Computer Science"), ("Software Engineering");
