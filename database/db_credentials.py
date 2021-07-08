bc3150fc8c198da334122af70cf32d66a12f5ae6
Anita <anitaly.ubc@gmail.com>
2021-07-07 21:30:56 -0700

    Created db_connector.py and db_credentials.py for database connection

---
+++ database/db_credentials.py
@@ -0,0 +1,11 @@
+# To actually have your app use this file, you need to RENAME the file to db_credentials.py
+# You will find details about your CS340 database credentials on Canvas.
+
+# the following will be used by the webapp.py to interact with the database
+# You can also use environment variables
+
+# For Local Development
+host = 'localhost'
+user = 'root'                  # can be different if you set up another username in your MySQL installation
+passwd = 'password'                        # set accordingly
+db = 'db'
