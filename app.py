4f974351b5a351d7f62223c51eb8bc92aa7d0e3d
Anita <lyani@oregonstate.edu>
2021-07-15 21:17:02 -0700

    Added POST

--- app.py
+++ app.py
@@ -24,12 +24,32 @@ def courses():
     results = cursor.fetchall()
     return render_template("courses.html", items=results)
 
-@app.route("/campuses")
+@app.route("/campuses", methods=["GET", "POST"])
 def campuses():
+    error = None
     query = "SELECT * FROM campuses;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     images = os.listdir(os.path.join(app.static_folder, "img"))
+    
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        campus = request.form['campus']
+        
+        campus_query = "SELECT DISTINCT campus_id, campus_name FROM campuses ORDER BY campus_id ASC;"
+        campus_cursor = db.execute_query(db_connection=db_connection, query=campus_query)
+        campus_results = campus_cursor.fetchall()
+        for dict in campus_results:
+            campus_name = dict.get('campus_name')
+            if campus_name == campus:
+                campus_id = dict.get('campus_id')
+                print(campus_id)
+        
+        insert_query = "INSERT INTO students(student_first_name, student_last_name, campus_id) VALUES (%s, %s, %s);"
+        data = (first_name, last_name, campus_id)
+        insert_cursor = db.execute_query(db_connection=db_connection, query=insert_query, query_params=data)
+
     return render_template("campuses.html", items=results, images=images)
 
 @app.route("/contact")
