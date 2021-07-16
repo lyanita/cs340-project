d70a8b793aceb531c95dd421904c574561fea4c9
Anita <lyani@oregonstate.edu>
2021-07-15 21:05:29 -0700

    Added new attributes for fields

--- database/campuses.sql
+++ database/campuses.sql
@@ -1,7 +1,7 @@
 DROP TABLE campuses;
 
 CREATE TABLE campuses(
-    campus_id INT PRIMARY KEY AUTO_INCREMENT,
+    campus_id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     campus_name VARCHAR(255) NOT NULL UNIQUE,
     campus_city TEXT,
     campus_country TEXT,
@@ -9,4 +9,6 @@ CREATE TABLE campuses(
 );
 
 INSERT INTO campuses(campus_name, campus_city, campus_country, campus_online)
-VALUES ("Corvallis", "Corvallis", "United States of America", TRUE), ("Davis", "Los Angeles", "United States of America", FALSE), ("Washington", "Seattle", "United States of America", TRUE);
+VALUES ("Corvallis", "Corvallis", "United States of America", TRUE), 
+("Davis", "Los Angeles", "United States of America", FALSE), 
+("Washington", "Seattle", "United States of America", TRUE);
