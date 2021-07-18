272e05eebb7b1f09a3782570c61148fb13d664f1
Anita <lyani@oregonstate.edu>
2021-07-18 12:35:35 -0700

    Moved .js files to static folder

---
+++ static/footer.js
@@ -0,0 +1,4 @@
+document.getElementById("footer").innerHTML =
+    'Copyright 2021 AH University</br>' +
+    'Contact Us: 1 (800) 333 - 1234'
+    ;
---
+++ static/header.js
@@ -0,0 +1,3 @@
+document.getElementById("header").innerHTML =
+    '<a href="index.html"><img class="logo" src="../static/img/logo.png"></a>'
+    ;
