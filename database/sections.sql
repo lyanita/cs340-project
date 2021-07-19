90bf9903b0540cbefa39d56533c82d03743751d6
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-07-19 12:29:58 -0700

    Updated columns, added sample data
    Removed max_occupancy, current_occupancy.
    Updated primary key syntax

--- database/sections.sql
+++ database/sections.sql
@@ -1,22 +1,51 @@
 DROP TABLE sections;
 
 CREATE TABLE sections(
-    section_id INT PRIMARY KEY AUTO_INCREMENT,
+    section_id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     course_id INT,
     instructor_id INT,
     campus_id INT,
-    max_occupancy INT NOT NULL,
-    current_occupancy INT NOT NULL,
-    CHECK (current_occupancy <= max_occupancy),
     FOREIGN KEY (course_id) REFERENCES courses(course_id),
     FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id),
     FOREIGN KEY (campus_id) REFERENCES campuses(campus_id)
 );
 
-INSERT INTO sections(course_id, instructor_id, campus_id, max_occupancy, current_occupancy) 
+INSERT INTO sections(course_id, instructor_id, campus_id) 
 VALUES
-    (SELECT course_id FROM courses WHERE course_name = "Introduction to Databases"),
-    (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Tommy" and instructor_last_name = "Pickles"),
-    (SELECT campus_id FROM campuses WHERE campus_id = "1"), 30
-    -- need to insert query for current_occupancy)
-)
+    (
+        (SELECT course_id FROM courses WHERE course_name = "Introduction to Databases"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Chuckie" and instructor_last_name = "Finster"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Corvallis")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Introduction to Databases"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Kimi" and instructor_last_name = "Finster"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Davis")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Introduction to Databases"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Edwin" and instructor_last_name = "Deville"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Washington")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Introduction to Computer Science"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Chuckie" and instructor_last_name = "Finster"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Corvallis")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Introduction to Computer Science"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Kimi" and instructor_last_name = "Finster"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Davis")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Introduction to Computer Science"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Edwin" and instructor_last_name = "Deville"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Washington")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Software Engineering"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Tommy" and instructor_last_name = "Pickles"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Corvallis")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Software Engineering"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Matt" and instructor_last_name = "Chung"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Davis")
+    ),(
+        (SELECT course_id FROM courses WHERE course_name = "Software Engineering"),
+        (SELECT instructor_id FROM instructors WHERE instructor_first_name = "Angel" and instructor_last_name = "Carmin"),
+        (SELECT campus_id FROM campuses WHERE campus_name = "Washington")
+    );
