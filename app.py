59f5d327d5179da21315a1f1dd251247c72f342e
Anita <lyani@oregonstate.edu>
2021-07-19 19:41:28 -0700

    Added instructor queries

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import datetime
 # Configuration
 app = Flask(__name__)
 app.permanent_session_lifetime = datetime.timedelta(days=365)
-db_connection = db.connect_to_database()
+#db_connection = db.connect_to_database()
 
 # Routes
 @app.route("/")
@@ -21,7 +21,7 @@ def root():
 def index():
     return render_template("index.html")
 
-@app.route("/campuses.html", methods=["GET", "POST", "DELETE"])
+@app.route("/campuses.html", methods=["GET", "POST"])
 def campuses():
     db_connection = db.connect_to_database()
     post_message = ""
@@ -54,7 +54,6 @@ def campuses():
         campus_results = campus_cursor.fetchall()
     
     db_connection.close()
-
     return render_template("campuses.html", items=campus_results, post_message=post_message, delete_message=delete_message)
 
 @app.route("/delete-campus/<int:id>")
@@ -66,16 +65,43 @@ def delete_campus(id):
     delete_message = "You have deleted campus id #" + str(id) + "."
 
     db_connection.close()
-
     return redirect("/campuses.html")
+
+@app.route("/instructors.html", methods=["GET", "POST"])
+def instructors():
+    db_connection = db.connect_to_database()
+    post_message = ""
+    instructor_query = "SELECT * FROM instructors ORDER BY instructor_id ASC;"
+    instructor_cursor = db.execute_query(db_connection=db_connection, query=instructor_query)
+    instructor_results = instructor_cursor.fetchall()
+
+    if request.method == "POST":
+        instructor_first_name = request.form['instructor_first_name']
+        instructor_last_name = request.form['instructor_last_name']
+        campus_id = request.form['campus_id']
+
+        insert_query = "INSERT INTO instructors(instructor_first_name, instructor_last_name, campus_id) VALUES (%s, %s, %s);"
+        data = (instructor_first_name, instructor_last_name, campus_id)
+        insert_cursor = db.execute_query(db_connection=db_connection, query=insert_query, query_params=data)
+        post_message = "You have successfully added a new instructor."
+
+    db_connection.close()    
+    return render_template("instructors.html", item=instructor_results, post_message=post_message)
+
+@app.route("/delete-instructor/<int:id>")
+def delete_instructor(id):
+    db_connection = db.connect_to_database()
+    delete_query = "DELETE FROM campuses WHERE instructor_id = %s;"
+    data = (id,)
+    delete_cursor = db.execute_query(db_connection=db_connection, query=delete_query, query_params=data)
+    delete_message = "You have deleted instructor id #" + str(id) + "."
+
+    db_connection.close()
+    return redirect("/instructors.html")
 
 @app.route("/sections.html")
 def sections():
     return render_template("sections.html")
-
-@app.route("/instructors.html")
-def instructors():
-    return render_template("instructors.html")
 
 @app.route("/courses.html")
 def courses():
@@ -89,8 +115,7 @@ def courses():
 @app.route("/students.html", methods=["GET", "POST"])
 def students():
     db_connection = db.connect_to_database()
-    error = None
-    message = ""
+    post_message = ""
     select_query = "SELECT * FROM campuses ORDER BY campus_id ASC;"
     select_cursor = db.execute_query(db_connection=db_connection, query=select_query)
     select_results = select_cursor.fetchall()
@@ -123,22 +148,21 @@ def students():
         register_results = register_cursor.fetchall()
         print(register_results)
         student_id = str(register_results[0].get('student_id') + 1)
-        message = "Thanks for registering, " + first_name + "! Your student ID is " + student_id + "."
+        post_message = "Thanks for registering, " + first_name + "! Your student ID is " + student_id + "."
 
     db_connection.close()
-
-    return render_template("students.html", items=select_results, images=images, count=student_result, message=message)
+    return render_template("students.html", items=select_results, images=images, count=student_result, post_message=post_message)
 
 @app.route("/contact.html")
 def contact():
     db_connection = db.connect_to_database()
-    query = "SELECT * FROM campuses ORDER BY campus_id ASC;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
+    campus_query = "SELECT * FROM campuses ORDER BY campus_id ASC;"
+    campus_cursor = db.execute_query(db_connection=db_connection, query=campus_query)
+    campus_results = campus_cursor.fetchall()
 
     coordinates_list = []
     locator = gp.Nominatim(user_agent="Geocoder")
-    for dict in results:
+    for dict in campus_results:
         campus_city = dict.get("campus_city")
         campus_name = dict.get("campus_name")
         location = locator.geocode(campus_city)
@@ -152,8 +176,7 @@ def contact():
             coordinates_list.append(coordinates)
     
     db_connection.close()
-
-    return render_template("contact.html", items=results, markers=json.dumps(coordinates_list))
+    return render_template("contact.html", items=campus_results, markers=json.dumps(coordinates_list))
 
 # Listener
 if __name__ == "__main__":
