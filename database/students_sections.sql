0cff5ed83afe2d07cd0bfa290fea4f269a599385
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-07-22 02:41:36 -0700

    Added primary key

--- database/students_sections.sql
+++ database/students_sections.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS students_sections;
 
 CREATE TABLE students_sections(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     student_id INT NOT NULL,
     section_id INT NOT NULL,
-    PRIMARY KEY(student_id, section_id),
     FOREIGN KEY(student_id) REFERENCES students(student_id),
     FOREIGN KEY(section_id) REFERENCES sections(section_id)
 );
