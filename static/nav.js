447b2c318cd05aea258b1f21bef9267021b629e9
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-07-27 00:49:28 -0700

    Add files via upload

---
+++ static/nav.js
@@ -0,0 +1,41 @@
+document.getElementById("navbar").innerHTML =
+    '<!-- navigation bar -->' +
+    '<div class="nav">' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn">Students</button>' +
+            '<div class="dropdown-content">' +
+              '<a href="students.html">Manage Students</a>' +
+              '<a href="add_students.html">Add Students</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn">Courses</button>' +
+            '<div class="dropdown-content">' +
+              '<a href="courses.html">Manage Courses</a>' +
+              '<a href="add_courses.html">AddCourses</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn">Sections</button>' +
+            '<div class="dropdown-content">' +
+              '<a href="sections.html">Manage Sections</a>' +
+              '<a href="add_sections.html">Add Sections</a>' +
+              '<a href="section-register.html">Section Registration</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn">Instructors</button>' +
+            '<div class="dropdown-content">' +
+              '<a href="instructors.html">Manage Instructors</a>' +
+              '<a href="add_instructors.html">Add Instructors</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn">Campuses</button>' +
+            '<div class="dropdown-content">' +
+              '<a href="campuses.html">Manage Campuses</a>' +
+              '<a href="add_campuses.html">Add Campuses</a>' +
+            '</div>' +
+          '</div>' +
+    '</div>'
+    ;
