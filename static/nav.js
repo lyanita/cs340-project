1cd2c6cdc319532cac3896eba9fe34786ee6df6a
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-07-27 22:31:47 -0700

    Added links to the main nav buttons

--- static/nav.js
+++ static/nav.js
@@ -2,21 +2,21 @@ document.getElementById("navbar").innerHTML =
     '<!-- navigation bar -->' +
     '<div class="nav">' +
           '<div class="dropdown">' +
-            '<button class="dropbtn">Students</button>' +
+            '<button class="dropbtn"><a href="students.html">Students</a></button>' +
             '<div class="dropdown-content">' +
               '<a href="students.html">Manage Students</a>' +
               '<a href="add_students.html">Add Students</a>' +
             '</div>' +
           '</div>' +
           '<div class="dropdown">' +
-            '<button class="dropbtn">Courses</button>' +
+            '<button class="dropbtn"><a href="courses.html">Courses</a></button>' +
             '<div class="dropdown-content">' +
               '<a href="courses.html">Manage Courses</a>' +
               '<a href="add_courses.html">Add Courses</a>' +
             '</div>' +
           '</div>' +
           '<div class="dropdown">' +
-            '<button class="dropbtn">Sections</button>' +
+            '<button class="dropbtn"><a href="sections.html">Sections</a></button>' +
             '<div class="dropdown-content">' +
               '<a href="sections.html">Manage Sections</a>' +
               '<a href="add_sections.html">Add Sections</a>' +
@@ -24,14 +24,14 @@ document.getElementById("navbar").innerHTML =
             '</div>' +
           '</div>' +
           '<div class="dropdown">' +
-            '<button class="dropbtn">Instructors</button>' +
+            '<button class="dropbtn"><a href="instructors.html">Instructors</a></button>' +
             '<div class="dropdown-content">' +
               '<a href="instructors.html">Manage Instructors</a>' +
               '<a href="add_instructors.html">Add Instructors</a>' +
             '</div>' +
           '</div>' +
           '<div class="dropdown">' +
-            '<button class="dropbtn">Campuses</button>' +
+            '<button class="dropbtn"><a href="campuses.html">Campuses</a></button>' +
             '<div class="dropdown-content">' +
               '<a href="campuses.html">Manage Campuses</a>' +
               '<a href="add_campuses.html">Add Campuses</a>' +
