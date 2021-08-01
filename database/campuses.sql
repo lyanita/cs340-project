027d3493fa589615481cbe313063ad4cc53017df
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-07-31 17:19:27 -0700

    Capitalized the first letter of the table names

--- database/campuses.sql
+++ database/campuses.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS campuses;
 
-CREATE TABLE campuses(
+CREATE TABLE Campuses(
     campus_id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     campus_name VARCHAR(255) NOT NULL UNIQUE,
     campus_city TEXT,
@@ -8,7 +8,7 @@ CREATE TABLE campuses(
     campus_online BOOLEAN
 ) ENGINE = InnoDB;
 
-INSERT INTO campuses(campus_name, campus_city, campus_country, campus_online)
+INSERT INTO Campuses(campus_name, campus_city, campus_country, campus_online)
 VALUES 
     ("Corvallis", "Corvallis", "United States of America", TRUE), 
     ("Davis", "Los Angeles", "United States of America", FALSE), 
