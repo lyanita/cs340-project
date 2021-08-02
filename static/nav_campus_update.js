13546e448e02b02842f3e562731592a144049deb
parkhyey <71689421+parkhyey@users.noreply.github.com>
2021-08-02 00:50:43 -0700

    Created nav_campus_update.js

---
+++ static/nav_campus_update.js
@@ -0,0 +1,41 @@
+document.getElementById("navbar").innerHTML =
+    '<!-- navigation bar -->' +
+    '<div class="nav">' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn"><a href="../students.html">Students</a></button>' +
+            '<div class="dropdown-content">' +
+              '<a href="../students.html">Manage Students</a>' +
+              '<a href="../add_students.html">Add Students</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn"><a href="../courses.html">Courses</a></button>' +
+            '<div class="dropdown-content">' +
+              '<a href="../courses.html">Manage Courses</a>' +
+              '<a href="../add_courses.html">Add Courses</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn"><a href="../sections.html">Sections</a></button>' +
+            '<div class="dropdown-content">' +
+              '<a href="../sections.html">Manage Sections</a>' +
+              '<a href="../add_sections.html">Add Sections</a>' +
+              '<a href="../section_register.html">Section Registration</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn"><a href="../instructors.html">Instructors</a></button>' +
+            '<div class="dropdown-content">' +
+              '<a href="../instructors.html">Manage Instructors</a>' +
+              '<a href="../add_instructors.html">Add Instructors</a>' +
+            '</div>' +
+          '</div>' +
+          '<div class="dropdown">' +
+            '<button class="dropbtn"><a href="../campuses.html">Campuses</a></button>' +
+            '<div class="dropdown-content">' +
+              '<a href="../campuses.html">Manage Campuses</a>' +
+              '<a href="../add_campuses.html">Add Campuses</a>' +
+            '</div>' +
+          '</div>' +
+    '</div>'
+    ;
